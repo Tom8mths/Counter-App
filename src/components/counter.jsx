5e0c3de38a917f0722bd7efd1f92d7be8d830b6e
Matheus Satelos <matheus.satelos@gmail.com>
2019-11-26 12:50:14 -0200

    incrementando navbar

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,59 +1,62 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.value,
-    tags: ["tag1", "tag2", "tag3"]
-  };
+  // state = {
+  //   value: this.props.counter.value,
+  //   tags: ["tag1", "tag2", "tag3"]
+  // };
 
-  constructor(props) {
-    super(props);
-    this.handleIncrement = this.handleIncrement.bind(this);
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
+  // renderTags() {
+  //   if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+  //   return (
+  //     <ul>
+  //       {this.state.tags.map(tag => (
+  //         <li key={tag}>{tag}</li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
 
-  handleIncrement = product => {
-    console.log(product);
-    this.setState({ value: this.state.value + 1 });
-  };
+  // handleIncrement = product => {
+  //   console.log(product);
+  //   this.setState({ value: this.state.value + 1 });
+  // };
 
-  doHandleIncrement = () => {
-    this.handleIncrement({ id: 1 });
-  };
+  // doHandleIncrement = () => {
+  //   this.handleIncrement({ id: 1 });
+  // };
 
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.doHandleIncrement({ id: 1 })}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        {this.renderTags()}
+        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">
+          Delete
+        </button>
+        {/* {this.renderTags()} */}
       </React.Fragment>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
     // return this.state.count === 0 ? 'Zero' : this.state.count;
   }
